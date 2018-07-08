c35cf40f09025d3a7e358343c03169ab016cf160
Glu2018Glu <weixinben163@163.com>
2018-07-08 13:29:15 +0800

    2.0

--- R/LCreat.R
+++ R/LCreat.R
@@ -19,32 +19,37 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 #,backend = "Ramp"
-LCreat <- function(DNalis,InL=1000,StT=0,EnT=1000000000000,MSn=1) {
+LCreat <- function(DNalis,InL,StT,EnT,MSn=1) {
   require(mzR);require(msdata);
+  if (missing(DNalis)) {DNalis<-list.files(getwd(),pattern="mzXML")} else {}
   mzOb<-list()
   DPa<-getwd()
-#sadf
+
   for (jj in 1 : length(DNalis)) {
     DNa<-DNalis[jj]
-  file<-list.files(DPa,pattern =DNa,full.names = TRUE,recursive =FALSE);
-  aa<-openMSfile(file);
-  LNa<-peaks(aa);
-  rtna<-header(aa);
-  rtna$msLevel[rtna$msLevel!=MSn]<-NA
-  rtna<-rtna[-23]
-  rtna<-na.omit(rtna)
-  rtna$retentionTime<-rtna$retentionTime/60
-  rtna$retentionTime[rtna$retentionTime<StT]<-NA
-  rtna$retentionTime[rtna$retentionTime>EnT]<-NA
-  rtna<-na.omit(rtna)
-  LNa<-LNa[rtna$seqNum]
-  names(LNa)<-rtna$retentionTime
-  ScNu<-as.numeric(length(LNa))
-  n=0;detL=0;oriL<-length(LNa);for (i in 1 : length(LNa)) {LNa[[i-detL]][,2][LNa[[i-detL]][,2]<InL]<-NA;detL<-oriL-length(LNa)}
-  n=0;detL=0;oriL<-length(LNa);for (i in 1 : length(LNa)) {LNa[[i-detL]]<-na.omit(LNa[[i-detL]]);detL<-oriL-length(LNa)}
-  mzOb[[jj]]<-LNa
+    file<-list.files(DPa,pattern =DNa,full.names = TRUE,recursive =FALSE);
+    aa<-openMSfile(file);
+    LNa<-peaks(aa);
+    rtna<-header(aa);
+    rtna$msLevel[rtna$msLevel!=MSn]<-NA
+    rtna<-rtna[-23]
+    rtna<-na.omit(rtna)
+    rtna$retentionTime<-rtna$retentionTime/60
+
+    if (missing(StT) | missing(EnT)) {} else {
+      rtna$retentionTime[rtna$retentionTime<StT]<-NA
+      rtna$retentionTime[rtna$retentionTime>EnT]<-NA}
+    rtna<-na.omit(rtna)
+    LNa<-LNa[rtna$seqNum]
+
+    names(LNa)<-rtna$retentionTime
+    ScNu<-as.numeric(length(LNa))
+    if (missing(InL)) {} else {
+      n=0;detL=0;oriL<-length(LNa);for (i in 1 : length(LNa)) {LNa[[i-detL]][,2][LNa[[i-detL]][,2]<InL]<-NA;detL<-oriL-length(LNa)}
+      n=0;detL=0;oriL<-length(LNa);for (i in 1 : length(LNa)) {LNa[[i-detL]]<-na.omit(LNa[[i-detL]]);detL<-oriL-length(LNa)}}
+    mzOb[[jj]]<-LNa
   }
   names(mzOb)<-DNalis
   save(mzOb,file = "mzOb.RDa")
-  return(0)
+  return()
 }
--- R/LXIC.R
+++ R/LXIC.R
@@ -16,46 +16,53 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-LXIC <- function(MZ,ppm=10) {
+LXIC <- function(RAWlis,MZ,ppm=10) {
   relist<-list()
   plolist<-list()
-  RAWlis<-load("mzOb.RDa")
-  #if (MZ==NULL) { MZ<-read.table("clipboard",header=F,sep=",")}
-  #MZ<-read.table("clipboard",header=F,sep=",")
+  load("mzOb.RDa")
+  if (missing(RAWlis)) {RAWlis<-mzOb} else {}
+  if (missing(MZ)) {MZK<-read.table("clipboard",header=F,sep=",")
+  MZ<-c()
+  for (ii in 1:length(MZK$V1)) {MZ[ii]<-as.double(MZK$V1[ii])}
+  } else {}
+
+
   Tolist<-matrix(nrow=length(MZ)*length(RAWlis),ncol = 4)
+  colnames(Tolist)<-c("Retention time","Mearsure M/z","Intensity","Sample Name")
   for (jk in 1:length(RAWlis)) {
     RAWDATA<-RAWlis[[jk]]
-  TopRDATA<-c()
-  for (kk in 1 : length(MZ)) {
-  Mzz<-as.numeric(MZ[kk])
-  MZMAX<-as.double(Mzz+Mzz*ppm/1000000)
-  MZMIN<-as.double(Mzz-Mzz*ppm/1000000)
-  RDATA<-RAWDATA
-  for (i in 1:length(RDATA)) RDATA[[i]][,1][RDATA[[i]][,1]>MZMAX|RDATA[[i]][,1]<MZMIN]<-NA
-  for (i in 1:length(RDATA)) RDATA[[i]]<-na.omit(RDATA[[i]])
-  n=0;detL=0;oriL<-length(RDATA);for (i in 1 : length(RDATA)) {if (length(RDATA[[i-detL]])<1) {RDATA[[i-detL]]<-NULL;detL<-detL+1}}
-  mz<-c()
-  Intensity<-c()
-RT<-signif(as.double(names(RDATA)),digits=2)
-  for (i in 1 : length(RDATA)) {mz[i]<-RDATA[[i]][1]}
-  for (i in 1 : length(RDATA)) {Intensity[i]<-RDATA[[i]][2]}
+    TopRDATA<-c()
+    for (kk in 1 : length(MZ)) {
+      Mzz<-as.numeric(MZ[kk])
+      MZMAX<-as.double(Mzz+Mzz*ppm/1000000)
+      MZMIN<-as.double(Mzz-Mzz*ppm/1000000)
+      RDATA<-RAWDATA
+      for (i in 1:length(RDATA)) RDATA[[i]][,1][RDATA[[i]][,1]>MZMAX|RDATA[[i]][,1]<MZMIN]<-NA
+      for (i in 1:length(RDATA)) RDATA[[i]]<-na.omit(RDATA[[i]])
+      n=0;detL=0;oriL<-length(RDATA);for (i in 1 : length(RDATA)) {if (length(RDATA[[i-detL]])<1) {RDATA[[i-detL]]<-NULL;detL<-detL+1}}
+      mz<-c()
+      Intensity<-c()
+      RT<-as.double(names(RDATA))
+      for (i in 1 : length(RDATA)) {mz[i]<-RDATA[[i]][1]}
+      for (i in 1 : length(RDATA)) {Intensity[i]<-RDATA[[i]][2]}
 
-  RDATA<-data.frame(RT,mz,Intensity)
- # relist[[kk]]<-RDATA
+      RDATA<-data.frame(RT,mz,Intensity)
+      relist[[kk]]<-RDATA
 
 
-  TopRDATA<-RDATA[order(-RDATA$Intensity),]
+      TopRDATA<-RDATA[order(-RDATA$Intensity),]
 
-  Tolist[kk+(jk-1)*3,1]<- TopRDATA[1,1]
-  Tolist[kk+(jk-1)*3,2]<-round(TopRDATA[1,2],digits = 4)
-  Tolist[kk+(jk-1)*3,3]<-format(TopRDATA[1,3],scientific = TRUE,digits = 2)
-  Tolist[kk+(jk-1)*3,4]<-names(RAWlis[jk])
-  }
+      Tolist[kk+(jk-1)*length(MZ),1]<- signif(TopRDATA[1,1],digits=2)
+      Tolist[kk+(jk-1)*length(MZ),2]<-round(TopRDATA[1,2],digits = 4)
+      Tolist[kk+(jk-1)*length(MZ),3]<-format(TopRDATA[1,3],scientific = TRUE,digits = 2)
+      Tolist[kk+(jk-1)*length(MZ),4]<-names(RAWlis[jk])
+    }
 
 
   }
   #plot(RDATA[,1],RDATA[,3],type="b",)
-  return(Tolist)
+  write.csv(Tolist,"temp.csv" )
+  return(writeClipboard(Tolist[,2]))
 
 
 }
