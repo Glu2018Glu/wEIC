99c1ca79dd79d39efd8c7f2c06a8ab5400ad319b
Glu2018Glu <weixinben163@163.com>
2018-07-10 00:45:51 +0800

    add EIC picture module

--- R/LXIC.R
+++ R/LXIC.R
@@ -17,6 +17,7 @@
 #   Test Package:              'Ctrl + Shift + T'
 
 LXIC <- function(RAWlis,MZ,ppm=10) {
+
   relist<-list()
   plolist<-list()
   load("mzOb.RDa")
@@ -31,6 +32,9 @@ LXIC <- function(RAWlis,MZ,ppm=10) {
   colnames(Tolist)<-c("Retention time","Mearsure M/z","Intensity","Sample Name")
   for (jk in 1:length(RAWlis)) {
     RAWDATA<-RAWlis[[jk]]
+    nc<-nchar(names(RAWlis[jk]))
+    if (dir.exists(substring(names(RAWlis[jk]),1,nc-6))) {} else
+    {dir.create(substring(names(RAWlis[jk]),1,nc-6))}
     TopRDATA<-c()
     for (kk in 1 : length(MZ)) {
       Mzz<-as.numeric(MZ[kk])
@@ -52,17 +56,28 @@ LXIC <- function(RAWlis,MZ,ppm=10) {
 
       TopRDATA<-RDATA[order(-RDATA$Intensity),]
 
-      Tolist[kk+(jk-1)*length(MZ),1]<- signif(TopRDATA[1,1],digits=2)
+      Tolist[kk+(jk-1)*length(MZ),1]<- round(TopRDATA[1,1],digits=1)
       Tolist[kk+(jk-1)*length(MZ),2]<-round(TopRDATA[1,2],digits = 4)
       Tolist[kk+(jk-1)*length(MZ),3]<-format(TopRDATA[1,3],scientific = TRUE,digits = 2)
       Tolist[kk+(jk-1)*length(MZ),4]<-names(RAWlis[jk])
+      wkwd<-getwd()
+      setwd(substring(names(RAWlis[jk]),1,nc-6))
+      jpeg(file=paste(MZ[kk],".jpeg",seq=""),width=1200,height=1000,res=56*2)
+      plot(RDATA$RT,RDATA$Intensity,type="b",)
+      title=paste("EIC of ",MZ[kk],seq="")
+      text(TopRDATA[1,1],TopRDATA[1,3],paste("RT= ",round(TopRDATA[1,1],digits=1),seq=""),pos=2,offset=3)
+      text(TopRDATA[1,1],TopRDATA[1,3],paste("Measured m/z= ",round(TopRDATA[1,2],digits = 4),seq=""),pos=4,offset=3)
+      dev.off()
+      setwd(wkwd)
+
     }
 
 
   }
-  #plot(RDATA[,1],RDATA[,3],type="b",)
-  write.csv(Tolist,"temp.csv" )
-  return(writeClipboard(Tolist[,2]))
 
+  TTo<-paste(Tolist[,2],Tolist[,1],Tolist[,3],Tolist[,4],sep = "\t")
+  write.csv(Tolist,"temp.csv" )
+  #return(writeClipboard(Tolist[,2]))
+  return(writeClipboard(TTo))
 
 }
