a0aaa0c051bd69ec92ade65c125f92500d8fb961
Glu2018Glu <weixinben163@163.com>
2018-11-26 00:59:14 +0800

    double charge and retention time refer

---
+++ R/FullLCreat.R
@@ -0,0 +1,57 @@
+# @title
+#LCreat
+#
+# @ description
+#creat list and filter ;preparation for EIC
+# @para
+#InL=1000 <intensity low limited>
+#StT=0 <start time>
+#EnT  <end time>
+#DPa <data path>
+#DNa/DNalis  <data name>/<data name list>
+#LNa <list name>
+#msn <MSLevel>
+#
+#@return
+#LNa as list
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+#,backend = "Ramp"
+FullLCreat <- function(DNalis=NULL,InL,MSn=1) {
+  require(mzR);
+  if (missing(DNalis)) {DNalis<-list.files(getwd(),pattern="mzXML")} else {}
+  mzOb<-list()
+  DPa<-getwd()
+
+  for (jj in 1 : length(DNalis)) {
+    DNa<-DNalis[jj]
+    file<-list.files(DPa,pattern =DNa,full.names = TRUE,recursive =FALSE);
+    aa<-openMSfile(file);
+    LNa<-peaks(aa);
+    rtna<-header(aa);
+    rtna$msLevel[rtna$msLevel!=MSn]<-NA
+    rtna<-rtna[-23]
+    rtna<-na.omit(rtna)
+    rtna$retentionTime<-rtna$retentionTime/60
+
+
+    LNa<-LNa[rtna$seqNum]
+
+
+    LNaa<-list()
+    for (iii in 1:length(LNa)){nu<-c(1:length(LNa[[iii]]));LNaa[[iii]]<-data.frame(LNa[[iii]][,1],LNa[[iii]][,2],rtna$retentionTime[iii],nu)}
+    LNa<-LNaa
+    names(LNa)<-rtna$seqNum
+    ScNu<-as.numeric(length(LNa))
+    if (missing(InL)) {} else {
+      n=0;detL=0;oriL<-length(LNa);for (i in 1 : length(LNa)) {LNa[[i-detL]][,2][LNa[[i-detL]][,2]<InL]<-NA;detL<-oriL-length(LNa)}
+      n=0;detL=0;oriL<-length(LNa);for (i in 1 : length(LNa)) {LNa[[i-detL]]<-na.omit(LNa[[i-detL]]);detL<-oriL-length(LNa)}}
+    mzOb[[jj]]<-LNa
+  }
+  names(mzOb)<-DNalis
+  save(mzOb,file = "mzOb.RDa")
+  return()
+}
---
+++ R/RTlimited2.R
@@ -0,0 +1,171 @@
+# @title
+#LXIC
+#
+# @ description
+#XIC from LCreat Data
+# @para
+#DATA <subject list from LCeat>
+#MZ <target m/z>
+#ppm <ppmolution>
+#LNa<-RDATA
+
+#Dfa as data.frame
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+RTlimited2 <- function(RAWlis,MZ,ppm=10,RTWin=3) {
+  require(reshape2)
+  require(plyr)
+  relist<-list()
+  plolist<-list()
+  load("mzOb.RDa")
+  options(stringsAsFactors = FALSE)
+  if (missing(RAWlis)) {RAWlis<-mzOb} else {}
+  if (missing(MZ))
+  {MZK<-read.delim2("clipboard",header=F);MZ<-c();
+  {if (length(MZK)==2)
+  {if (length(unlist(strsplit(as.character(MZK$V1[1])[1],",")))==2)
+  {
+    MZ<-matrix(nrow=length(MZK$V1),ncol = 3)
+    for (ii in 1:length(MZK$V1)) {lisi<-c();lisi<-unlist(strsplit(as.character(MZK$V1[ii])[1],","));MZ[ii,]<-c(lisi[1],lisi[2],as.double(MZK$V2[ii]))}
+  }
+    else
+    { MZ<-matrix(nrow=length(MZK$V1),ncol = 2)
+    for (ii in 1:length(MZK$V1)) {MZ[ii,]<-c(as.double(MZK$V1[ii]),as.double(MZK$V2[ii]))}
+    }
+  }
+    else
+    {if (length(unlist(strsplit(as.character(MZK$V1[1])[1],",")))==2)
+    {
+      MZ<-matrix(nrow=length(MZK$V1),ncol = 2)
+      for (ii in 1:length(MZK$V1)) {lisi<-c();lisi<-unlist(strsplit(as.character(MZK$V1[ii])[1],","));MZ[ii,]<-c(lisi[1],lisi[2])}
+    }
+      else
+      {MZ<-as.matrix(MZK)
+
+      }
+
+    }
+
+  }
+  }
+  else
+  {}
+
+  Tolist<-matrix(nrow=nrow(MZ)*length(RAWlis),ncol = 4)
+  colnames(Tolist)<-c("Retention time","Mearsure M/z","Intensity","Sample Name")
+  #Sample list (RAWlis[[jk]]) cycle begain
+  for (jk in 1:length(RAWlis)) {
+    RAWDATA<-RAWlis[[jk]]
+    nc<-nchar(names(RAWlis[jk]))
+    # Creat sample folder (RAWlis[jk])
+    if (dir.exists(substring(names(RAWlis[jk]),1,nc-6))) {} else
+    {dir.create(substring(names(RAWlis[jk]),1,nc-6))}
+    TopRDATA<-c()
+    #MZ list cycle begain (MZ[kk])
+    for (kk in 1 : nrow(MZ)) {
+      #dataset (RAWDATA) initialization
+      RDATA<-RAWDATA
+      Mzz<-as.double(MZ[kk,])
+      MZMAX1<-c()
+      MZMIN1<-c()
+      MZMAX1<-Mzz[1]+Mzz[1]*ppm/1000000
+      MZMIN1<-Mzz[1]-Mzz[1]*ppm/1000000
+
+
+      #MZ range got
+      #filtering accroding to MZ range
+      {if (length(unlist(strsplit(as.character(MZK$V1[1])[1],",")))==2)
+      {
+        MZMAX2<-c()
+        MZMIN2<-c()
+        MZMAX2<-Mzz[2]+Mzz[2]*ppm/1000000
+        MZMIN2<-Mzz[2]-Mzz[2]*ppm/1000000
+        RDATA<-llply(RDATA,function(x){x[,1][((x[,1]>MZMAX1|x[,1]<MZMIN1) )&( (x[,1]>MZMAX2|x[,1]<MZMIN2))]<-NA;return(x)})
+      }
+        else
+        {RDATA<-llply(RDATA,function(x){x[,1][x[,1]>MZMAX1|x[,1]<MZMIN1]<-NA;return(x)})}
+      }
+      #delete NA
+      RDATA<-llply(RDATA,function(x){x<-na.omit(x);return(x)})
+      n=0;detL=0;oriL<-length(RDATA);for (i in 1 : oriL) {if (nrow(RDATA[[i-detL]])<1) {RDATA[[i-detL]]<-NULL;detL<-detL+1}}
+      #filtering Finished
+
+
+
+
+
+
+
+
+      {if (length(unlist(strsplit(as.character(MZK$V1[1])[1],",")))==2){
+        #spectrum list of mz1
+        RDATA1<-llply(RDATA,function(x){x[,1][(x[,1]>MZMAX1|x[,1]<MZMIN1)]<-NA;return(x)})
+        RDATA1<-llply(RDATA1,function(x){x<-na.omit(x);return(x)})
+        n=0;detL=0;oriL<-length(RDATA1);for (i in 1 : oriL) {if (nrow(RDATA1[[i-detL]])<1) {RDATA1[[i-detL]]<-NULL;detL<-detL+1}}
+
+        RDATA1<-ldply(RDATA1)
+
+        colnames(RDATA1)<-c("ScN","mz","Intensity","RT","SpN")
+        #spectrum list of mz2
+        RDATA2<-llply(RDATA,function(x){x[,1][(x[,1]>MZMAX2|x[,1]<MZMIN2)]<-NA;return(x)})
+        RDATA2<-llply(RDATA2,function(x){x<-na.omit(x);return(x)})
+        n=0;detL=0;oriL<-length(RDATA2);for (i in 1 : oriL) {if (nrow(RDATA2[[i-detL]])<1) {RDATA2[[i-detL]]<-NULL;detL<-detL+1}}
+
+        RDATA2<-ldply(RDATA2)
+
+        colnames(RDATA2)<-c("ScN","mz","Intensity","RT","SpN")
+
+      } else {}}
+
+      RDATART<-RDATA
+      #filtering accroding to RT range
+      {if (length(MZK)==2)
+      {RTMIN<-c()
+      RTMAX<-c()
+      {if (length(unlist(strsplit(as.character(MZK$V1[1])[1],",")))==2) rt<-Mzz[3] else rt<-Mzz[2]}
+      RTMAX<-rt+RTWin
+      RTMIN<-rt-RTWin
+      RDATART<-llply(RDATART,function(x){x[,3][x[,3]>RTMAX|x[,3]<RTMIN]<-NA;return(x)})
+      }
+        else {}
+
+      }
+      #delete NA
+      RDATART<-llply(RDATART,function(x){x<-na.omit(x);return(x)})
+      n=0;detL=0;oriL<-length(RDATART);for (i in 1 :length(RDATART)) {if (nrow(RDATART[[i-detL]])<1) {RDATART[[i-detL]]<-NULL;detL<-detL+1}}
+
+
+
+      RDATART<-ldply(RDATART)
+
+
+      colnames(RDATART)<-c("ScN","mz","Intensity","RT","SpN")
+      RDATART<-RDATART[-5]
+      md<-melt(RDATART,id=c("ScN","mz","RT"))
+      new<-dcast(md,ScN+mz+RT~variable,sum)
+      TopRDATA<-new[order(-new$Intensity),]
+
+      Tolist[kk+(jk-1)*nrow(MZ),1]<- round(TopRDATA[1,3],digits=1)
+      intmz<-RDATART[RDATART$ScN==TopRDATA[1,1],]
+      Tolist[kk+(jk-1)*nrow(MZ),2]<-intmz[order(-intmz$Intensity),][1,2]
+      Tolist[kk+(jk-1)*nrow(MZ),3]<-format(TopRDATA[1,4],scientific = TRUE,digits = 2)
+
+      Tolist[kk+(jk-1)*nrow(MZ),4]<-names(RAWlis[jk])
+
+
+
+
+      write.csv(Tolist,"temp.csv" )
+
+      #return(writeClipboard(Tolist[,2]))
+
+
+    }
+  }
+  TTo<-paste(Tolist[,1],Tolist[,2],Tolist[,3],Tolist[,4],sep = "\t")
+  return(writeClipboard(TTo))
+}
